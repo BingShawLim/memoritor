23f8dd82746cd10d74ed043455a19e164d6113c0
Bing Shaw Lim <shaw.a.muvee@gmail.com>
2019-12-18 15:21:49 +0000

    2 tests done

--- spec/controllers/tasks_controller_spec.rb
+++ spec/controllers/tasks_controller_spec.rb
@@ -1,17 +1,13 @@
 require 'rails_helper'
-
-
 RSpec.describe TasksController, type: :controller do
-    
-    describe "tasks#index action" do
-    it "should successfully show the page" do
-        get :index
-        expect(response).to have_http_status(:success)
-        end
-    end
-
     describe "tasks#index" do
         it "should list the tasks in the database" do
+            task1 = FactoryBot.create(:task)
+            task2 = FactoryBot.create(:task)
+            get :index
+            expect(response).to have_http_status(:success)
+            response_value = ActiveSupport::JSON.decode(@response.body)
+            expect(response_value.count).to eq(2)
         end
     end
 
